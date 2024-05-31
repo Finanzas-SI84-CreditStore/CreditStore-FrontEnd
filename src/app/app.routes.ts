1e3e29fe344465409ee6b242cf9df648b7767706
LucianRuiz <luciano.software@hotmail.com>
2024-05-30 23:40:47 -0500

    feat: Implementation of the feature home and the navbar-side

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './features/home/components/home-page/home-page.component';
+import {AddCustomerComponent} from "./features/clients/components/add-customer/add-customer.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },

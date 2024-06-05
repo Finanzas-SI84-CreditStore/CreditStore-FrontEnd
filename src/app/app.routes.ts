b2f33a144b25da6a686bef0a7e4284072c0f0743
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2024-06-05 02:25:37 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { TheLoginPageComponent } from './features/the-login-page/the-login-page.component';
 import { HomePageComponent } from './features/home/components/home-page/home-page.component';
-import {AddCustomerComponent} from "./features/clients/components/add-customer/add-customer.component";
-
+import { AddCustomerComponent } from './features/clients/components/add-customer/add-customer.component';
+import { AddPaymentComponent } from './features/add-payment/add-payment.component';
+import { TheCreateAccountComponent } from './features/the-create-account/the-create-account.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-  { path: 'inicio', component: AddCustomerComponent },
-  // Agrega otras rutas aquí
+  { path: 'inicio', component: TheLoginPageComponent },
+  { path: 'dashboard', component: HomePageComponent },
+  { path: 'addClient', component: AddCustomerComponent },
+  { path: 'addPayment', component: AddPaymentComponent },
+  { path: 'createAccount', component: TheCreateAccountComponent },
 ];

605f829d2e540ac48b4409d3483dbb2994240f30
LucianRuiz <luciano.software@hotmail.com>
2024-06-05 01:12:06 -0500

    feat: add client-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './features/home/components/home-page/home-page.component';
 import {AddCustomerComponent} from "./features/clients/components/add-customer/add-customer.component";
+import {ClientPageComponent} from "./features/clients/pages/client-page/client-page.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
